c2c4eaffdc2527edb67a8f7ad80ea840df924d40
Isa Vil <isa.vil@free.fr>
2018-05-04 16:17:19 +0200

    Classe abstraite + implémentation de l'héritage

---
+++ classe/VoitureSansPermis.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 04/05/2018
+ * Time: 14:44
+ */
+
+class VoitureSansPermis
+{
+
+    public function __construct()
+    {
+        echo "La voiturette de golf!";
+    }
+
+    public function count($a){
+        echo $a;
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
+
+/*namespace loulou;
+
+function date(){
+    echo"Coucou";
+
+}
+
+date();
+
+*/
 date_default_timezone_set('Europe/Paris');
 
 require_once "./function/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
 
 
-$voiture = new Berline("Mini", "Cooper");
-var_dump($voiture);
+$nullos = new VoitureSansPermis();
+
+
+
+
